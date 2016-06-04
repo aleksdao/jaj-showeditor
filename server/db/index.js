7fbdd4a0dbb15765f3767efc07b5a20c443919c2
Alex Dao <rahx1t@gmail.com>
2016-06-04 10:59:42 -0400

    fix db/index.js to connect to database_uri

--- server/db/index.js
+++ server/db/index.js
@@ -13,7 +13,7 @@ function connect () {
 	if (_conn)
 		return _conn
 	_conn = new Promise(function (resolve, reject) {
-		mongoose.connect('mongodb://localhost/jaj-showeditor', function (err){
+		mongoose.connect(DATABASE_URI, function (err){
 			if(err)
 				return reject(err);
 			resolve(mongoose.connection);
