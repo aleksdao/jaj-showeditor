7544998752ebbd16cb432a6c2135936d572007e5
Alex Dao <rahx1t@gmail.com>
2016-06-04 10:08:04 -0400

    adding env vars and pushing to heroku

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,14 @@
 var mongoose = require('mongoose');
 var Promise = require('bluebird');
+var path = require('path');
 
 require('./models/event.js');
 require('./models/show.js');
 
 var _conn;
+var DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
+
+
 function connect () {
 	if (_conn)
 		return _conn
---
+++ server/env/production.js
@@ -0,0 +1,3 @@
+module.exports = {
+  "DATABASE_URI" : process.env.MONGOLAB_URI
+}
